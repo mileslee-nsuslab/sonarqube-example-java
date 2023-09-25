9202a54cf0e957a4e6a6b352d62a8d332533bf94
miles.lee <miles.lee@nsuslab.com>
2023-09-25 17:21:44 +0900

    fix: add duplicate code

--- src/main/javascript/type_inference.js
+++ src/main/javascript/type_inference.js
@@ -24,3 +24,14 @@ function compute() {
 
   return total.foo();
 }
+
+function compute() {
+  var total = 0;
+  for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; i++) {
+      total = i+ j;
+    }
+  }
+
+  return total.foo();
+}
