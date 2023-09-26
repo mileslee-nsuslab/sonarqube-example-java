787348a06e6913ff17fce4112bfd33231465fa87
miles.lee <miles.lee@nsuslab.com>
2023-09-26 11:39:21 +0900

    fix: add duplicate code

--- src/main/javascript/type_inference.js
+++ src/main/javascript/type_inference.js
@@ -14,3 +14,56 @@ function sum(a, b) {
   return a + b;
 }
 
+function compute() {
+  var total = 0;
+  for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; i++) {
+      total = i+ j;
+    }
+  }
+
+  return total.foo();
+}
+
+function sum(a, b) {
+  return a + b;
+}function compute() {
+  var total = 0;
+  for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; i++) {
+      total = i+ j;
+    }
+  }
+
+  return total.foo();
+}
+
+function sum(a, b) {
+  return a + b;
+}function compute() {
+  var total = 0;
+  for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; i++) {
+      total = i+ j;
+    }
+  }
+
+  return total.foo();
+}
+
+function sum(a, b) {
+  return a + b;
+}function compute() {
+  var total = 0;
+  for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; i++) {
+      total = i+ j;
+    }
+  }
+
+  return total.foo();
+}
+
+function sum(a, b) {
+  return a + b;
+}
